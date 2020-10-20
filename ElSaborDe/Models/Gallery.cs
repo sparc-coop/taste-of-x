540b4b9b1176fcf65c23f5cd5ed233d71699c1b7
Dante Elegante <dante@kuviocreative.com>
2020-10-20 13:11:17 -0500

    Update to El Sabor de

---
+++ ElSaborDe/Models/Gallery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElSaborDe.Models
+{
+    public class Gallery
+    {
+        public string ImageUrl { get; set;}
+    }
+}
---
+++ ElSaborDe/Models/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElSaborDe.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string[] Category { get; set; }
+
+        public string[] Cuisine { get; set; }
+        public string OpeningHours { get; set; }
+        public string ClosingHoours { get; set; }
+        public string[] DaysOpen { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PhoneNumber { get; set; }
+        public string About { get; set; }
+    }
+}
